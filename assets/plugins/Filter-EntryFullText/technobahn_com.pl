8fcf814357a04dfb46d70c660f8298bae479bc8e
Shinsuke Matsui <poppen.jp@gmail.com>
2009-03-27 14:51:10 +0900

    added EFT for http://www.technobahn.com/

---
+++ assets/plugins/Filter-EntryFullText/technobahn_com.pl
@@ -0,0 +1,26 @@
+use Plagger::Util qw( decode_content );
+
+sub handle {
+    my($self, $args) = @_;
+    return $args->{entry}->link =~ m|http://www\.technobahn\.com/news/\d+|;
+}
+
+sub extract {
+    my($self, $args) = @_;
+    my $content;
+
+    if (my ($url) =
+        $args->{content} =~ m|<form name="myFORM" action="([^"]*)"|) {
+        my $ua = Plagger::UserAgent->new;
+        my $res = $ua->post($url, { continue => 'y' });
+        return if $res->is_error;
+        $content = decode_content($res->content);
+    } else {
+        $content = $args->{content};
+    }
+
+    if ($content =~ m|<td bgcolor="#ffffff" height="10%" valign=top>(.*?)</td></tr><tr><td><img src="http://img.yahoo.co.jp/images/clear.gif" width=620 height=10>|ms) {
+        return $1;
+    }
+    return;
+}
